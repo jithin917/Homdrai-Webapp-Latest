008eaadc598dcf239aebf49ab1e26da6e7918e49
jithin917 <jithin917@gmail.com>
2025-06-30 14:10:22 +0530

    Fix OMSLogin import/export mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { AIStylistChat } from './pages/AIStylistChat';
 import { SavedDesigns } from './pages/SavedDesigns';
 import Auth from './pages/Auth';
 import { OrderTracking } from './pages/oms/OrderTracking';
-import { OMSLogin } from './pages/oms/OMSLogin';
+import OMSLogin from './pages/oms/OMSLogin';
 import OMSDashboard from './pages/oms/OMSDashboard';
 import { AdminLogin } from './pages/admin/AdminLogin';
 import { AdminPanel } from './pages/admin/AdminPanel';
